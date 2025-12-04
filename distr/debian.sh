78b163a11e00af54157a9f175299bf0f13f321c6
Ace Of Snakes <aceofsnakesmain@gmail.com>
2025-12-04 23:44:27 +0300

    add scripts for more smart build

--- distr/debian.sh
+++ distr/debian.sh
@@ -1,3 +1,13 @@
-./build
-pwd
-dpkg-buildpackage -b -uc -us -d
+#!/bin/bash 
+
+PWDFROM=.
+
+. ./prepare.sh
+echo "--->" $PWDFROM
+cd $PWDFROM
+
+echo "==============================================================="
+echo $0 PWD: $(pwd)
+echo "==============================================================="
+
+dpkg-buildpackage -B -uc -us 
