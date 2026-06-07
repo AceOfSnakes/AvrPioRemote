683ec7c09acd7e036f45b54ef9fad3c1dbba4231
Ace Of Snakes <aceofsnakesmain@gmail.com>
2026-06-07 21:40:54 +0300

    removw window flag

--- src/aboutdialog.cpp
+++ src/aboutdialog.cpp
@@ -63,7 +63,6 @@ AboutDialog::AboutDialog(QWidget *parent) :
     QDialog(parent),
     ui(new Ui::AboutDialog)
 {
-    this->setWindowFlags(Qt::Window | Qt::WindowCloseButtonHint);
     ui->setupUi(this);
     this->setFixedSize(this->size());
     QString compiler;
